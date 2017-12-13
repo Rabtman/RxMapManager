374c45d5f7fe6923a4bcc3b9f093d46641da7448
rabtman <zhangjm05@gmail.com>
2017-12-13 08:59:58 +0800

    1. 完善demo 2. 更新readme 3. 添加preview

--- README.md
+++ README.md
@@ -1,1 +1,118 @@
-#RxMapManager
+# RxMapManager
+
+easy to use map by rxjava.
+
+## Features
+
+1. rxjava2
+2. integrate baidu map and amap
+3. location-info can be received anywhere
+
+## Download
+
+Maven:
+
+```xml
+<dependency>
+  <groupId>com.rabtman.rxmapmanager</groupId>
+  <artifactId>rxmapmanager</artifactId>
+  <version>0.1.0</version>
+  <type>pom</type>
+</dependency>
+```
+
+or Gradle:
+
+```groovy
+compile 'com.rabtman.rxmapmanager:rxmapmanager:0.1.0'
+```
+
+## How to use
+
+Init rxmapmanager:
+
+```
+//use baidu map
+RxMapManager.getInstance().init(new BaiDuMapStrategy() {
+      @Override
+      public LocationClient initClient() {
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
+        option.setCoorType("bd09ll");
+        option.setScanSpan(5000);
+        option.setOpenGps(true);
+        option.setIsNeedAddress(true);
+        option.setNeedDeviceDirect(true);
+        LocationClient locationClient = new LocationClient(getBaseContext());
+        locationClient.setLocOption(option);
+        return locationClient;
+      }
+    });
+    
+//or use amap
+RxMapManager.getInstance().init(new AMapStrategy() {
+      @Override
+      public AMapLocationClient initClient() {
+        AMapLocationClientOption option = new AMapLocationClientOption();
+        option.setNeedAddress(true);
+        option.setInterval(5000);
+        AMapLocationClient client = new AMapLocationClient(getBaseContext());
+        client.setLocationOption(option);
+        return client;
+      }
+    });
+```
+
+
+Before receiving location-info, you need to start:
+
+```
+RxMapManager.getInstance().onStart();
+```
+
+Keep receiving location-info:
+
+```
+RxMapManager.getInstance().requestLocation();
+```
+
+Get the last location-info:
+
+```
+RxMapManager.getInstance().getLastLocation();
+```
+
+
+Need to stop receiving, you can:
+
+```
+RxMapManager.getInstance().onStop();
+```
+
+Release resources:
+
+```
+RxMapManager.getInstance().destory();
+```
+
+
+## Preview
+
+![](https://github.com/Rabtman/RxMapManager/raw/master/screenshots/rxmapdemo.gif)
+
+## License
+
+```
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+```
+
--- app/src/main/java/com/rabtman/rxmapdemo/MainActivity.java
+++ app/src/main/java/com/rabtman/rxmapdemo/MainActivity.java
@@ -2,10 +2,12 @@ package com.rabtman.rxmapdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.widget.TextView;
 import com.amap.api.location.AMapLocation;
 import com.amap.api.location.AMapLocationClient;
@@ -20,23 +22,129 @@ import io.reactivex.functions.Consumer;
 
 public class MainActivity extends AppCompatActivity {
 
-  private Button start, stop, get;
+  private Button start, stop, get, clear;
+  private RadioGroup selectMap;
+  private RadioButton selectBaidu, selectAmap;
   private TextView result;
+  //当前选择的地图平台
+  private int currentMap;
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-    setSupportActionBar(toolbar);
 
+    selectMap = findViewById(R.id.map_group);
+    selectBaidu = findViewById(R.id.rbtn_baidu);
+    selectAmap = findViewById(R.id.rbtn_amap);
     start = findViewById(R.id.btn_start);
     stop = findViewById(R.id.btn_stop);
     get = findViewById(R.id.btn_get);
+    clear = findViewById(R.id.btn_clear);
     result = findViewById(R.id.tv_result);
 
-    //useAMap();
+    selectMap.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+      @Override
+      public void onCheckedChanged(RadioGroup group, int checkedId) {
+        if (checkedId == currentMap) {
+          return;
+        }
+
+        currentMap = checkedId;
+        if (selectBaidu.getId() == checkedId) {
+          result.append("  选择了：百度地图\n");
+          useBaiDu();
+        } else if (selectAmap.getId() == checkedId) {
+          result.append("  选择了：高德地图\n");
+          useAMap();
+        }
+      }
+    });
+
+    clear.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        result.setText("");
+      }
+    });
+
+    //开始定位
+    start.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        selectBaidu.setEnabled(false);
+        selectAmap.setEnabled(false);
+        RxMapManager.getInstance().onStart();
+      }
+    });
+
+    //停止定位
+    stop.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        selectBaidu.setEnabled(true);
+        selectAmap.setEnabled(true);
+        RxMapManager.getInstance().onStop();
+      }
+    });
+
+    //获取最近一次定位信息
+    get.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        RxMapManager.getInstance().getLastLocation()
+            .subscribe(new Consumer<Object>() {
+              @Override
+              public void accept(Object o) throws Exception {
+                if (o == null) {
+                  result.append("【主动获取定位】 null\n");
+                } else {
+                  if (selectBaidu.getId() == currentMap) {
+                    result.append("【主动获取定位】lat:" + ((BDLocation) o).getLatitude()
+                        + "，lng:" + ((BDLocation) o).getLongitude() + "\n");
+                  } else if (selectAmap.getId() == currentMap) {
+                    result.append("【主动获取定位】lat:" + ((AMapLocation) o).getLatitude()
+                        + "，lng:" + ((AMapLocation) o).getLongitude() + "\n");
+                  }
+                }
+              }
+            }, new Consumer<Throwable>() {
+              @Override
+              public void accept(Throwable throwable) throws Exception {
+                throwable.printStackTrace();
+              }
+            });
+      }
+    });
+
+    init();
+  }
+
+
+  private void init() {
+    currentMap = selectBaidu.getId();
     useBaiDu();
+  }
+
+  //使用百度定位
+  private void useBaiDu() {
+    RxMapManager.getInstance().init(new BaiDuMapStrategy() {
+      @Override
+      public LocationClient initClient() {
+        LocationClientOption option = new LocationClientOption();
+        // Hight_Accuracy高精度、Battery_Saving低功耗、Device_Sensors仅设备(GPS)
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);// 设置定位模式
+        option.setCoorType("bd09ll");// 返回的定位结果是百度经纬度,默认值gcj02
+        option.setScanSpan(5000);// 设置发起定位请求的间隔时间为5000ms
+        option.setOpenGps(true);// 打开gps
+        option.setIsNeedAddress(true);// 返回的定位结果包含地址信息
+        option.setNeedDeviceDirect(true);// 返回的定位结果包含手机机头的方向
+        LocationClient locationClient = new LocationClient(getBaseContext());
+        locationClient.setLocOption(option);
+        return locationClient;
+      }
+    });
 
     //接收定位信息
     RxMapManager.getInstance().requestLocation()
@@ -58,62 +166,6 @@ public class MainActivity extends AppCompatActivity {
         });
   }
 
-  //使用百度定位
-  private void useBaiDu() {
-    RxMapManager.getInstance().init(new BaiDuMapStrategy() {
-      @Override
-      public LocationClient initClient() {
-        LocationClientOption option = new LocationClientOption();
-        // Hight_Accuracy高精度、Battery_Saving低功耗、Device_Sensors仅设备(GPS)
-        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);// 设置定位模式
-        option.setCoorType("bd09ll");// 返回的定位结果是百度经纬度,默认值gcj02
-        option.setScanSpan(5000);// 设置发起定位请求的间隔时间为5000ms
-        option.setOpenGps(true);// 打开gps
-        option.setIsNeedAddress(true);// 返回的定位结果包含地址信息
-        option.setNeedDeviceDirect(true);// 返回的定位结果包含手机机头的方向
-
-        LocationClient locationClient = new LocationClient(getBaseContext());
-        locationClient.setLocOption(option);
-
-        return locationClient;
-      }
-    });
-    start.setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        RxMapManager.getInstance().onStart();
-      }
-    });
-    stop.setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        RxMapManager.getInstance().onStop();
-      }
-    });
-    get.setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        RxMapManager.getInstance().getLastLocation()
-            .subscribe(new Consumer<BDLocation>() {
-              @Override
-              public void accept(BDLocation o) throws Exception {
-                if (o == null) {
-                  result.append("【主动获取定位】 null\n");
-                } else {
-                  result.append("【主动获取定位】lat:" + o.getLatitude()
-                      + "，lng:" + o.getLongitude() + "\n");
-                }
-              }
-            }, new Consumer<Throwable>() {
-              @Override
-              public void accept(Throwable throwable) throws Exception {
-                throwable.printStackTrace();
-              }
-            });
-      }
-    });
-  }
-
   //使用高德定位
   private void useAMap() {
     RxMapManager.getInstance().init(new AMapStrategy() {
@@ -122,47 +174,35 @@ public class MainActivity extends AppCompatActivity {
         AMapLocationClientOption option = new AMapLocationClientOption();
         option.setNeedAddress(true);
         option.setInterval(5000);
-
         AMapLocationClient client = new AMapLocationClient(getBaseContext());
         client.setLocationOption(option);
-
         return client;
       }
     });
-    start.setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        RxMapManager.getInstance().onStart();
-      }
-    });
-    stop.setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        RxMapManager.getInstance().onStop();
-      }
-    });
-    get.setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        RxMapManager.getInstance().getLastLocation()
-            .subscribe(new Consumer<AMapLocation>() {
-              @Override
-              public void accept(AMapLocation o) throws Exception {
-                if (o == null) {
-                  result.append("【主动获取定位】 null\n");
-                } else {
-                  result.append("【主动获取定位】lat:" + o.getLatitude()
-                      + "，lng:" + o.getLongitude() + "\n");
-                }
-              }
-            }, new Consumer<Throwable>() {
-              @Override
-              public void accept(Throwable throwable) throws Exception {
-                throwable.printStackTrace();
-              }
-            });
-      }
-    });
+
+    //接收定位信息
+    RxMapManager.getInstance().requestLocation()
+        .subscribe(new Consumer<AMapLocation>() {
+          @Override
+          public void accept(AMapLocation o) throws Exception {
+            if (o == null) {
+              result.append("【接收定位信息】 null\n");
+            } else {
+              result.append("【接收定位信息】lat:" + o.getLatitude()
+                  + "，lng:" + o.getLongitude() + "\n");
+            }
+          }
+        }, new Consumer<Throwable>() {
+          @Override
+          public void accept(Throwable throwable) throws Exception {
+            throwable.printStackTrace();
+          }
+        });
   }
 
+  @Override
+  protected void onDestroy() {
+    RxMapManager.getInstance().destory();
+    super.onDestroy();
+  }
 }
